d35117f54c4a9b39aeffe63db98ba8a6cfa27128
kyosukeyashiro <ijel.yashiro@icloud.com>
2020-09-19 20:41:32 +0900

    レビュー機能

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,12 +2,14 @@ class PlayerController < ApplicationController
 def index
     @player=Player.find_by(id: params[:id])
     @comments=Comment.where player: @player.name
+    @comment=Comment.new
+    @review=@comments.all.average(:review)
 end
 def create
     @player=Player.find_by(id: params[:id])
     @comment=Comment.new
     @comment.content=params[:content]
-    @comment.review=params[:review]
+    @comment.review=params[:review].to_f
     @comment.player=@player.name
     @user=User.find_by(id: session[:user_id])
     if session[:user_id]
